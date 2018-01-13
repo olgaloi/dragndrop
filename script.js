54508489af9315a46a8be30034527518e7ad22aa
Olga Loisha <olga.lojsza@gmail.com>
2018-01-13 14:00:30 +0300

    Implemented event delegation and position setting

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-let elem = document.querySelector('div');
+const div = document.querySelector('div');
 
-elem.onmousedown = function(event) {
-	console.log(event.target, 'mousedown');
+div.onmousedown = function(event) {
+	if(event.target.tagName != 'IMG') return;
+
+	for(let elem of div.children) {
+		if(elem != event.target) {
+			elem.style.position = 'static'
+		}
+	}
+
+	drag(event.target);
+}
+
+function drag(target) {
+	target.style.position = 'relative';
 }
