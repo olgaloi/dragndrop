c2bf89e30789aa1108945cfaf6219fbbe3a430df
Olga Loisha <olga.lojsza@gmail.com>
2018-01-14 18:54:02 +0300

    Fixed coordinates calculation

--- script.js
+++ script.js
@@ -3,22 +3,20 @@ const div = document.querySelector('div');
 div.onmousedown = function(event) {
 	if(event.target.tagName != 'IMG') return;
 
-	/*for(let elem of div.children) {
-		if(elem != event.target) {
-			elem.style.position = 'static';
-		}
-	}*/
-
 	drag(event);
 }
 
 function drag(event) {
-	const target = event.target;
+	let target = event.target;
 	target.style.position = 'relative';
 
-	let coords = getCoords(target);
-	let shiftX = event.pageX - coords.left;
-	let shiftY = event.pageY - coords.top;
+	// координаты верхнего угла перетаскиваемого элемента
+	let offsetX = target.style.left;
+	let offsetY = target.style.top;
+
+	// координаты клика мышки
+	let startX = event.pageX;
+	let startY = event.pageY;
 
 	target.ondragstart =  function() {
 		return false;
@@ -29,21 +27,14 @@ function drag(event) {
 	}
 
 	function moveAt(e) {
-		target.style.left = e.pageX - shiftX + 'px';
-		target.style.top = e.pageY - shiftY + 'px';
+		target.style.left = offsetX + e.pageX - startX + 'px';
+		target.style.top = offsetY + e.pageY - startY + 'px';
 	}
 
 	target.onmouseup = function(e) {
 		div.onmousemove = null;
-		target.onmouseup = null
-	}
-
-	function getCoords(e) {   // кроме IE8-
-		const box = e.getBoundingClientRect();
-
-		return {
-			top: box.top + pageYOffset,
-			left: box.left + pageXOffset
-		};
+		target.onmouseup = null;
+		target.ondragstart = null;
+		target = null;
 	}
 }
